17f0fbd7f0a053cb22925a83af4e0be3c12a70d7
Pavellko <pavelkollegov@mail.ru>
2022-09-08 11:05:21 +0300

    v1

---
+++ primer.py
@@ -0,0 +1,23 @@
+from kivy.app import App
+from kivy.uix.button  import Button
+from kivy.uix.label  import Label
+from kivy.uix.boxlayout  import BoxLayout
+
+
+class MyApp(App):
+    def build(self):
+        lb = Label(text="Это надпись")
+        btn = Button(text='Это кнопка')
+        box = BoxLayout(orientation ='vertical',padding=8, spacing=8)
+
+        box.add_widget(lb)
+        box.add_widget(btn)
+
+        btn.on_press = self.funk
+        return box
+
+    def funk(self):
+        print('Нажатие')
+
+app = MyApp()
+app.run()
