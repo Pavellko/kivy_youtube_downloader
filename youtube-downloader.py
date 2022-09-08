b69782a56d466e87b8b65ce3242a0f1096d31570
Pavellko <pavelkollegov@mail.ru>
2022-09-08 11:09:14 +0300

    v2

--- youtube-downloader.py
+++ youtube-downloader.py
@@ -6,7 +6,6 @@ from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 from kivy.core.window import Window
-from kivy.uix.scrollview import ScrollView
 from kivy.uix.image import Image
 
 from pytube import YouTube
